6a22190f4590cb5ba8c81cfb2744661e8f88aabf
naebofcso <wolff.jacob3@gmail.com>
2013-05-21 12:53:58 -0700

    First commit.

---
+++ calcRocketHeight/src/com/wolff/RocketMain.java
@@ -0,0 +1,23 @@
+package com.wolff;
+
+import java.util.Scanner;
+import java.math.*;
+
+public class RocketMain 
+{
+	private int distanceFromLaunch;
+	
+	public RocketMain()
+	{}
+	
+	public static void main(String[] args)
+	{
+		
+	}
+	
+	public int calcHeight(int dist)
+	{
+		
+		return;
+	}
+}
